509b6348cd8c0fa3d993b8deb079f9d22ad8c6c2
agb2k <hcyab2@nottingham.edu.my>
2021-11-17 19:53:03 +0800

    Added functionality and prepared for deployment

--- Telegram.py
+++ Telegram.py
@@ -8,37 +8,70 @@ import gspread
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-sa = gspread.service_account(filename="service_account.json")
-
+# Checks if connection to Google Sheets API is possible
 try:
-    sheet = sa.open("Orders")
+    serviceAccount = gspread.service_account(filename="service_account.json")
+
+    sheet = serviceAccount.open("Orders")
 
     malaySheet = sheet.worksheet("Malay")
     mamakSheet = sheet.worksheet("Mamak")
     beverageSheet = sheet.worksheet("Beverage")
     koreanSheet = sheet.worksheet("Korean")
     japaneseSheet = sheet.worksheet("Japanese")
+    commentSheet = sheet.worksheet("Comments")
 
     print(f"Successfully connected to {sheet.title} Google Sheets")
 except ConnectionError:
     print("ERROR: Google Sheets Connection Error")
 
 
+# The default message that takes place when user sends a message
 def start(update: Update, context: CallbackContext) -> None:
-    """The default message that takes place when user sends a message"""
     user = update.effective_user
     update.message.reply_markdown_v2(
         fr'Hi {user.mention_markdown_v2()}\! Send me a command to get started\! Use /help to check them out\.',
     )
 
 
+# Send a message when the command /help is issued.
 def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('List of commands:\n/order - Order dishes\n/menu - See menu\n')
+    update.message.reply_text(
+        'List of commands:\n/order - Order dishes\n/menu - See menu\n/contact - Get help from someone\n/comment - '
+        'Give us your constructive criticism')
 
 
+# Bot sends a message when the command /menu is issued.
+def menu_command(update: Update, context: CallbackContext) -> None:
+    context.bot.sendDocument(update.effective_chat.id,
+                             document=open("FreeLoadersMenu.pdf", 'rb'), caption=f'Here\'s our menu!')
+
+
+# Bot sends a message when the command /contact is issued.
+def contact_command(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Chatbot Creator - abhinav.basil@gmail.com\nCampus Services - '
+                              'campus.services@nottingham.edu.my')
+
+
+# Bot sends a message when the command /comment is issued
+def comment_command(update: Update, context: CallbackContext) -> None:
+    comment = str(' '.join(context.args))
+
+    if comment:
+        try:
+            commentSheet.append_row([comment])
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text=f"Your comment has been submitted. Thank you!")
+        except:
+            update.message.reply_text("ERROR: Issue Appending")
+    else:
+        update.message.reply_text(
+            "Submit your anonymous comment by using the command, /comment, followed by your comment.\neg. /comment "
+            "The creator of this bot is awesome!")
+
+
+# Bot sends a message when the command /order is issued
 def order_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /order is issued."""
     user = update.effective_user.username
 
     try:
@@ -50,36 +83,23 @@ def order_command(update: Update, context: CallbackContext) -> None:
 
         if 1 <= int(item) <= 13:
             malaySheet.append_row(orderInfo)
-            context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text=f"Order successful! Item No. {item} will be sent to {room} for {user} from the Malay Stall"
-            )
         elif 14 <= int(item) <= 32:
             mamakSheet.append_row(orderInfo)
-            context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text=f"Order successful! Item No. {item} will be sent to {room} for {user} from the Mamak Stall"
-            )
         elif 33 <= int(item) <= 47:
             beverageSheet.append_row(orderInfo)
-            context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text=f"Order successful! Item No. {item} will be sent to {room} for {user} from the Beverage Stall"
-            )
         elif 48 <= int(item) <= 62:
             koreanSheet.append_row(orderInfo)
-            context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text=f"Order successful! Item No. {item} will be sent to {room} for {user} from the Korean Stall"
-            )
         elif 63 <= int(item) <= 87:
             japaneseSheet.append_row(orderInfo)
+
+        if 1 > int(item) > 87:
+            update.message.reply_text("Incorrect Order Number! Please Try Again")
+        else:
             context.bot.send_message(
                 chat_id=update.effective_chat.id,
-                text=f"Order successful! Item No. {item} will be sent to {room} for {user} from the Japanese Stall"
+                text=f"Order successful! Item No. {item} will be sent to {room} for {user}"
             )
-        else:
-            update.message.reply_text("Incorrect Order Number! Please Try Again")
+
 
     except:
         update.message.reply_text(
@@ -87,26 +107,23 @@ def order_command(update: Update, context: CallbackContext) -> None:
             f'29 J3B10')
 
 
-def menu_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /menu is issued."""
-    context.bot.sendDocument(update.effective_chat.id, document=open("FreeLoadersMenu.pdf", 'rb'),
-                             caption=f'Here\'s our menu!')
-
-
+# Starts the telegram bot
 def main() -> None:
-    """Start the bot."""
     # Create the Updater and pass it your bot's token.
+    # This should be a secret but it's left open for demo/testing purposes
     updater = Updater("2120331172:AAFbAfLx8V-deJIcyIfd_OJ6NbM1pBqrRAI")
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
-    # on different commands - answer in Telegram
+    # Different commands
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(CommandHandler("order", order_command))
     dispatcher.add_handler(CommandHandler("menu", menu_command))
+    dispatcher.add_handler(CommandHandler("contact", contact_command))
+    dispatcher.add_handler(CommandHandler("comment", comment_command))
 
-    # on non-commands
+    # Non-commands
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, start))
 
     # Start the Bot
